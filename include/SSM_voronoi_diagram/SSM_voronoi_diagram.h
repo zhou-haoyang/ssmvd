3ae501cf0e56fab3c19fb1272e676dfd64e17ad9
Howyoung <howyoung3@gmail.com>
2024-09-13 22:42:40 +0200

    feat: wip: btrace: find bisector

--- include/SSM_voronoi_diagram/SSM_voronoi_diagram.h
+++ include/SSM_voronoi_diagram/SSM_voronoi_diagram.h
@@ -36,6 +36,7 @@ class SSM_voronoi_diagram {
     using FT = typename Traits::FT;
     using Point_2 = typename Traits::Point_2;
     using Vector_2 = typename Traits::Vector_2;
+    using Line_2 = typename Traits::Line_2;
     using Polygon_2 = typename Traits::Polygon_2;
     using Parametric_line_2 = typename Traits::Parametric_line_2;
 
@@ -113,16 +114,16 @@ class SSM_voronoi_diagram {
         std::optional<FT> tmax;
     };
 
-    struct Segment_cone_intervals;
+    struct Intervals;
 
-    class Segment_cone_intervals_iterator {
+    class Intervals_iterator {
        public:
         using iterator_category = std::forward_iterator_tag;
-        using value_type = typename Segment_cone_intervals::value_type;
+        using value_type = typename Intervals::value_type;
         using difference_type = std::ptrdiff_t;
-        using iterator = Segment_cone_intervals_iterator;
+        using iterator = Intervals_iterator;
 
-        Segment_cone_intervals_iterator(Segment_cone_intervals* data, std::size_t idx) : data(data), idx(idx) {}
+        Intervals_iterator(Intervals* data, std::size_t idx) : data(data), idx(idx) {}
 
         value_type operator*() const { return (*data)[idx]; }
 
@@ -140,13 +141,13 @@ class SSM_voronoi_diagram {
         bool operator==(const iterator& other) const { return data == other.data && idx == other.idx; }
 
        private:
-        Segment_cone_intervals* data = nullptr;
+        Intervals* data = nullptr;
         std::size_t idx;
     };
 
-    struct Segment_cone_intervals {
+    struct Intervals {
         using value_type = std::pair<Metric_edge_iterator, Cone_line_intersection>;
-        using iterator = Segment_cone_intervals_iterator;
+        using iterator = Intervals_iterator;
 
         value_type operator[](std::size_t idx) const {
             CGAL_assertion(is_valid() && idx < num_cones());
@@ -213,7 +214,7 @@ class SSM_voronoi_diagram {
             ts.push_back(t);
         }
 
-        void clip(FT tmin, FT tmax, Segment_cone_intervals& res) const {
+        void clip(FT tmin, FT tmax, Intervals& res) const {
             res.clear();
 
             // Empty cases
@@ -277,9 +278,72 @@ class SSM_voronoi_diagram {
         return d_min;
     }
 
-    auto trace_boundary(Metric_edge_iterator ed, Cone_descriptor k0) {
+    auto trace_boundary(Metric_edge_iterator ed, Cone_descriptor k0, vd_vertex_descriptor prev_vd,
+                        bool add_border_edges = false, bool add_cone_vertices = false,
+                        vd_face_descriptor fd0 = vd_graph_traits::null_face(),
+                        vd_face_descriptor fd1 = vd_graph_traits::null_face()) {
         auto b_line = construct_parametric_line(*ed);
         FT tmin = 0, tmax = 1;
+
+        // Intervals of boundary segment clipped by cones of all sites
+        find_all_intervals(b_line, m_intervals_vec_cache, tmin, tmax);
+        auto& intervals_map = m_intervals_vec_cache;
+        auto& intervals = intervals_map[site_index(k0.site())];
+        auto interval_it = intervals.find_cone(k0.edge());
+        CGAL_assertion_msg(interval_it != intervals.end(), "Initial cone k0 not found in intervals");
+
+        Cone_descriptor k_prev;
+        for (;;) {
+            // Find the nearest two site bisector
+            FT dist_min, tb_min;
+            Line_2 bi_line_min;
+            std::vector<Cone_descriptor> k1_min;
+            bool found = false;
+            for (auto site_it = m_sites.begin(); site_it != m_sites.end(); ++site_it) {
+                if (site_it == k0.site()) continue;
+
+                auto interval = *interval_it;
+                FT tmin_overlap = max(interval.t_min(), tmin);
+                FT tmax_overlap = min(interval.t_max(), tmax);
+                intervals_map[site_index(site_it)].clip(tmin_overlap, tmax_overlap, m_intervals_cache);
+                auto& intervals_overlap = m_intervals_cache;
+
+                for (auto& [ed, interval_overlap] : intervals_overlap) {
+                    Cone_descriptor k1(site_it, ed);
+                    if (k1 == k_prev) continue;
+
+                    Line_2 bi_line = get_bisector(k0, k1);
+                    if (is_degenerate(bi_line)) continue;
+
+                    auto isect = intersect(b_line, bi_line);
+                    if (!isect || std::holds_alternative<Colinear>(*isect)) continue;
+
+                    FT tb = std::get<FT>(*isect);
+                    // TODO: marginal case: tb is on the terminal points
+                    if (tb <= interval_overlap.tmin() || tb > interval_overlap.tmax()) continue;
+
+                    FT dist = tb - tmin;
+                    CGAL_assertion_msg(dist >= 0, "Intersection must be on the segment");
+                    if (dist < dist_min || !found) {
+                        dist_min = dist;
+                        tb_min = tb;
+                        bi_line_min = bi_line;
+                        k1_min.clear();
+                        k1_min.push_back(k1);
+                        found = true;
+                        break;  // goto next site
+                    } else if (dist == dist_min) {
+                        k1_min.push_back(k1);
+                    }
+                }
+            }
+
+            if (!found) {
+                // No intersection found
+
+            } else {
+            }
+        }
     }
 
    protected:
@@ -288,21 +352,36 @@ class SSM_voronoi_diagram {
     Site_list m_sites;
     Traits m_traits;
 
+    Intervals m_intervals_cache;
+    std::vector<Intervals> m_intervals_vec_cache;
+
     TRAIT_FUNC(Vector_2, construct_vector, construct_vector_2_object)
     TRAIT_FUNC(Point_2, construct_point, construct_point_2_object)
     TRAIT_FUNC(Point_2, construct_point_on, construct_point_on_2_object)
     TRAIT_FUNC(Point_2, construct_source, construct_source_2_object)
     TRAIT_FUNC(Point_2, construct_target, construct_target_2_object)
     TRAIT_FUNC(Parametric_line_2, construct_parametric_line, construct_parametric_line_2_object)
+    TRAIT_FUNC(Line_2, construct_line, construct_line_2_object)
+
     TRAIT_FUNC(FT, squared_distance, compute_squared_distance_2_object)
     TRAIT_FUNC(FT, determinant, compute_determinant_2_object)
+    TRAIT_FUNC(FT, ca, compute_a_2_object)
+    TRAIT_FUNC(FT, cb, compute_b_2_object)
+    TRAIT_FUNC(FT, cc, compute_c_2_object)
+    TRAIT_FUNC(FT, cx, compute_x_2_object)
+    TRAIT_FUNC(FT, cy, compute_y_2_object)
+    TRAIT_FUNC(FT, scalar_product, compute_scalar_product_2_object)
+
+    TRAIT_FUNC(bool, is_degenerate, is_degenerate_2_object)
 
     struct Colinear {
         // operator =
         bool operator==(const Colinear&) const { return true; }
-    };
+    };  // namespace CGAL::SSM_voronoi_diagram
 
     using Parametric_line_intersection = std::variant<std::pair<FT, FT>, Colinear>;
+
+    std::size_t site_index(Site_iterator site) const { return std::distance(m_sites.begin(), site); }
 
     /**
      * @brief Find the intersection of a parametric line with a ray with direction d and starting at the origin.
@@ -338,7 +417,7 @@ class SSM_voronoi_diagram {
         return isect;
     }
 
-    auto find_intervals(Site_iterator site, const Parametric_line_2& segment, Segment_cone_intervals& res, FT tmin,
+    auto find_intervals(Site_iterator site, const Parametric_line_2& segment, Intervals& res, FT tmin,
                         std::optional<FT> tmax = std::nullopt) {
         res.clear();
 
@@ -433,6 +512,26 @@ class SSM_voronoi_diagram {
             res.add_edge(ed0);
         }
         if (tmax) res.add_intersection(*tmax);
+    }
+
+    void find_all_intervals(const Parametric_line_2& segment, std::vector<Intervals>& res, FT tmin,
+                            std::optional<FT> tmax = std::nullopt) {
+        res.resize(m_sites.size());
+        auto it = res.begin();
+        for (auto site_it = m_sites.begin(); site_it != m_sites.end(); ++site_it, ++it) {
+            find_intervals(site_it, segment, *it, tmin, tmax);
+        }
+    }
+
+    Vector_2 orthogonal_vertor(const Line_2& l) { return construct_vector(ca(l), cb(l)); }
+
+    Line_2 get_bisector(const Cone_descriptor& k0, const Cone_descriptor& k1) {
+        Line_2 l0 = construct_line(*(k0.edge())), l1 = construct_line(*(k1.edge()));
+        Vector_2 nd0 = orthogonal_vertor(l0) / abs(cc(l0)), nd1 = orthogonal_vertor(l1) / abs(cc(l1));
+        Vector_2 AB = nd0 - nd1;
+        FT C = scalar_product(nd1, construct_vector(ORIGIN, k0.site()->point())) -
+               scalar_product(nd0, construct_vector(ORIGIN, k1.site()->point()));
+        return construct_line(cx(AB), cy(AB), C);
     }
 };
 }  // namespace CGAL::SSM_voronoi_diagram
